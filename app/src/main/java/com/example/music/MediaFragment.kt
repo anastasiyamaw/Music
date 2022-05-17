1b9257e0f2af25ea98af4a758ee0a020ef9a9a48
Anastasiia <nasteneka_ua100@mail.ru>
2022-05-17 20:47:53 +0300

    реализация загрузки трека

--- app/src/main/java/com/example/music/MediaFragment.kt
+++ app/src/main/java/com/example/music/MediaFragment.kt
@@ -1,7 +1,11 @@
 package com.example.music
 
+import android.app.DownloadManager
+import android.content.Context
 import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
@@ -14,12 +18,22 @@ import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class MediaFragment : Fragment() {
 
     private val allTracks = arrayListOf(R.raw.track2, R.raw.track3,
         R.raw.track4, R.raw.track5, R.raw.track6,
         R.raw.track7, R.raw.track8, R.raw.track9)
 
+    private val dlsongs = arrayOf(
+        "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1uu2PDXQpf61vfIFUepbE13qBNaHknBFy",
+        "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1wWS2AaCnjUHxMCQTeYXJ9h17mcAadzIc",
+        "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1wQzzmV_rOAcguL7VVJ65Fs-gLdymawQe",
+        "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=15oD3BWZJaHjs0B7Km4c_HEzodu4XRmex",
+        "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1x62kWYCOnmqga2kRCwBW5-QNOH-ikVEj",
+        "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1HInO8uxdS66HRdBMYqcBDW2KcH_TTLrP",
+        "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1f0FdmAcRvNn5GB1smRJpwcgn_yQdIqe0",
+        "https://drive.google.com/uc?export=download&confirm=no_antivirus&id=1WBelRqrJIw0MUGUneAunw2GR9Iqwt-bX")
 
     lateinit var runnable: Runnable
     private var handler = Handler()
@@ -94,6 +108,9 @@ class MediaFragment : Fragment() {
         binding.swipemusic.setOnClickListener{
             findNavController().navigate(R.id.action_mediaFragment_to_secondFragment)
         }
+        binding.downloadTrack.setOnClickListener{
+            downloads(position)
+        }
 
         return binding.root
     }
@@ -103,5 +120,15 @@ class MediaFragment : Fragment() {
         return format.format(Date(time.toLong()))
     }
 
+    fun downloads(position: Int){
+        val request = DownloadManager.Request(Uri.parse(dlsongs[position]))
+            .setTitle("Track")
+            .setDescription("Downloading..")
+            .setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+            .setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "Track")
+            .setAllowedOverMetered(true)
 
+        val dm = context?.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+        dm.enqueue(request)
+    }
 }
